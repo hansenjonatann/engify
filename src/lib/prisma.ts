a6307393a7fd14f428b77c4b0f5a680e28602afb
hansenjonatann <hansenjonatann@gmail.com>
2026-05-26 15:13:37 +0700

    fix: fixing error again at prisma.ts in folder lib

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,9 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_ });
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
